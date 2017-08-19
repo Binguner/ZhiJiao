ef9eb3b74adbbd7ab31c9ee374717f0f4dfed24e
zhengzuoyu <zhengzuoyu0414@gmail.com>
2017-08-19 15:30:55 +0800

    fix ui

--- app/src/main/java/com/example/binguner/zhijiao/UI/LoginActivity.java
+++ app/src/main/java/com/example/binguner/zhijiao/UI/LoginActivity.java
@@ -15,15 +15,14 @@ import com.example.binguner.zhijiao.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Toolbar logintoolbar;
-    private Button loginbtn;
+    private Button loginbtn,canclebtn;
     private TextInputLayout username_textinputlayout,password_textinputlayout;
     private EditText ed_username,ed_password;
     private String username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme_Base);
+        setTheme(R.style.AppTheme_login);
         setContentView(R.layout.activity_login);
         initId();
         initViews();
@@ -31,69 +30,48 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void setListener() {
-        logintoolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        canclebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                username = username_textinputlayout.getEditText().getText().toString();
+                password = password_textinputlayout.getEditText().getText().toString();
                 Toast.makeText(LoginActivity.this,"Clicked "+username+" "+password,Toast.LENGTH_SHORT).show();
+                if(username.isEmpty()){
+                    username_textinputlayout.setError("Please input the username !");
+                }
+                if(password.isEmpty()){
+                    password_textinputlayout.setError("Please input the password !");
+                }else {
+                    username_textinputlayout.setErrorEnabled(false);
+                    password_textinputlayout.setErrorEnabled(false);
+                }
             }
         });
 
-        ed_username.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                username = editable.toString();
-            }
-        });
-
-        ed_password.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                password = editable.toString();
-            }
-        });
 
     }
 
     private void initViews() {
-        logintoolbar.setNavigationIcon(R.mipmap.back_white_48);
         username_textinputlayout.setHint("Username");
         password_textinputlayout.setHint("Password");
-       // username_textinputlayout.set
     }
 
     private void initId() {
+        canclebtn = findViewById(R.id.canclebtn);
         ed_password = findViewById(R.id.ed_password);
         ed_username = findViewById(R.id.ed_username);
         loginbtn = findViewById(R.id.loginbtn);
         password_textinputlayout = findViewById(R.id.password_textinputlayout);
         username_textinputlayout = findViewById(R.id.username_textinputlayout);
-        logintoolbar = findViewById(R.id.logintoolbar);
     }
 }
